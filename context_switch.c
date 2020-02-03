41292c31e7c36981bd228fd1e97172b8a1f38e74
Gavin Le <quocle@mail.usf.edu>
2020-02-02 21:23:58 -0500

    Update Context Switch and System Call
    Add Comments

--- context_switch.c
+++ context_switch.c
@@ -1,89 +1,117 @@
 #define _GNU_SOURCE
 #include <stdio.h>	
-#include <stdint.h>	
 #include <stdlib.h>	
 #include <time.h>	
-#include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
-#include <sys/time.h>
-#include <sys/types.h> 
 #include <sched.h>
-#include <pthread.h>
-#include <math.h>
-#include <signal.h>
-
 
 #define STRING_SIZE 100
 #define NUM_TRIAL 10000
 
 int main() 
 { 
+	//Create variables
 	struct timespec begin, end;
 	unsigned int i, timeInterval;
 
+	//This variable is used to store parent and child ID for testing purpose
+	int childID, parentID;
+
+	//Create string to passing between parent and child	
 	char stringParent[STRING_SIZE] = "This message from parent to child";
 	char stringChild[STRING_SIZE] = "This message from child to parent";
 
+	//Create parent and child pipe with two ends, READ and WRITE
+	//Where 0 is READ end and 1 is WRITE end.
 	int parentPid[2];
 	int childPid[2];
-	int childID, parentID;
+	//Connect two pipe together.
 	pipe(parentPid);
 	pipe(childPid);
-
+	//Start the processor
 	pid_t pid = fork();
 
+	//If pid < 0 pipe is fail
 	if (pid < 0) {
-		fprintf(stderr, "FORK FAILED\n");
+		fprintf(stderr, "PIPE FAILED\n");
         exit(1);
 	}
 
+	//Create cpu_set name mask
 	cpu_set_t mask;
-	CPU_ZERO (&mask); 
+	//Clear cpu
+	CPU_ZERO (&mask);
+	//Set cpu at 0
 	CPU_SET (0, &mask);
+	//Get a process's CPU affinity mask
 	sched_setaffinity(pid, sizeof(cpu_set_t), &mask);
 	
-	clock_gettime(CLOCK_MONOTONIC, &begin);
+	//If pid < 0, fork() is fail
 	if(pid < 0){
 		fprintf(stderr,"***ERROR: forking child process failed.\n");
 		exit(1);
 	}
 	else {
+		//Start timing by store current time as beginning.
+		clock_gettime(CLOCK_MONOTONIC, &begin);
+		//Start the loop, run for 10,000 times
 		for (i = 0; i < NUM_TRIAL; i++){
+			//if pid = 0, this is Child process
 			if (pid == 0) {		
+				//Get child ID
 				childID = getpid();
+				//Create string variable to read string from parent
 				char receivedString[STRING_SIZE];
-
+				//Close child Write end to start READING process
 				close(childPid[1]);
+				//Read message from Parent
 				read(childPid[0], receivedString, STRING_SIZE);
+				//Close Read end
 				close(childPid[0]);
-
+				
+				//Close parent reading end to start WRITING process
 				close(parentPid[0]);
+				//Write message to parent
 				write(parentPid[1], stringParent, strlen(stringParent)+1);
+				//Close parent writing end
 				close(parentPid[1]);
 
+				//Test output:
 				//printf("Child ID is %d, I receive this message from Parent = %s\n", childID, receivedString);
 			}
+			//Else, this is Parent process
 			else {
+				//Get parent ID
 				parentID = getpid();
+				//Create string variable to read string from child
 				char receivedString[STRING_SIZE];
 
+				//Close child reading end to start WRITING process
 				close(childPid[0]);
+				//Write message to child
 				write(childPid[1], stringChild, strlen(stringChild)+1);
+				//Close child writing end
 				close(childPid[1]);
 
+				//Close parent writing end to start READING process
 				close(parentPid[1]);
+				//Read message from child
 				read(parentPid[0], receivedString, STRING_SIZE);
+				//Close Parent reading end
 				close(parentPid[0]);
 
+				//Test output
 				//printf("Parent ID is %d, I receive this message from Child = %s\n",parentID, receivedString);
 			}
+			//Stop the clock by recording the ending time
 			clock_gettime(CLOCK_MONOTONIC, &end);
 		}
 	} 
-	
+	//Subtract ending time to beginning time to get time Interval
 	timeInterval = (end.tv_sec - begin.tv_sec)*1e9 + (end.tv_nsec - begin.tv_nsec);
+	//Print out result
     printf("Time Interval = %.2u ns\n", timeInterval);
+	//Divide time interval by number of trials to find the cost fo Context Switch
 	printf("The cost of Context Switch is %.3lf ns/trial\n", (double)timeInterval/(double)NUM_TRIAL);
     return 0;
 } 
--- system_call.c
+++ system_call.c
@@ -4,22 +4,33 @@
 
 int main()
 {
+    //Declare variable
     unsigned int NUM_TRIAL = 10000000;
     unsigned int i, timeInterval;
     struct timeval temp;
     struct timespec begin, end;
 
+    //Get the present time and store as begin time
     clock_gettime(CLOCK_MONOTONIC, &begin);
+
+    //Run the programe for 10,000,000 times
     for (i = 0; i < NUM_TRIAL; ++i) {
         gettimeofday(&temp, NULL);
     }
+
+    //Get the present time and store as begin time
     clock_gettime(CLOCK_MONOTONIC, &end);
 
+    //Subtract the end time with the begin to get the runtime of the for loop.
     timeInterval = (end.tv_sec - begin.tv_sec)*1e9 + (end.tv_nsec - begin.tv_nsec);
+
+    //Print out the result
     printf("Number of Calls = %u\n", NUM_TRIAL);
     printf("Time Start = %.2f ns\n", begin.tv_sec*1e9 + begin.tv_nsec);
     printf("time End = %.2f ns\n", end.tv_sec*1e9 + end.tv_nsec);
     printf("Time Interval = %.2u ns\n", timeInterval);
+
+    //Divide Time Interval by Number of Trials to get the cost of gettimeofday() System Call
     printf("The cost of System Call: %.3lf ns/trial \n", (double)timeInterval/(double)NUM_TRIAL);
 
     return 0;
